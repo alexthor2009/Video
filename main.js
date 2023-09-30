912bc755ae3626b0f329cfbc4468fd6cb5ec7171
alexthor2009 <113790589+alexthor2009@users.noreply.github.com>
2023-09-30 10:41:28 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+video="";
+status="";
+
+function proload(){
+    video=createVideo('video.mp4');
+    video.hide();
+}
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status:detectando objetos";
+}
+
+function modelLoaded(){
+    console.log("Modelo Cargado");
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
